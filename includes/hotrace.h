e1f57ea85a2c73860e427ec111a5eba082f5367d
Christophe Mehay <cmehay@student.42.fr>
2013-12-14 06:03:32 +0100

    working hard

--- includes/hotrace.h
+++ includes/hotrace.h
@@ -6,7 +6,7 @@
 /*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/13 23:48:37 by cmehay            #+#    #+#             */
-/*   Updated: 2013/12/14 02:08:38 by cmehay           ###   ########.fr       */
+/*   Updated: 2013/12/14 05:08:00 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # define ROTATE_X 5
 # define ROTATE_Y 2
 
+
 typedef enum	e_bool
 {
 	FALSE,
@@ -27,9 +28,20 @@ typedef enum	e_bool
 
 typedef struct	s_keyword
 {
-	int					key;
+	unsigned int		key;
 	char				*value;
 	struct s_keyword	*next;
 }				t_keyword;
+
+
+/*
+** CRC32
+*/
+typedef uint8_t	t_crc;
+# define POLY 0xD8
+# define WIDTH  (8 * sizeof(crc))
+# define TOPBIT (1 << (WIDTH - 1))
+
+
 
 #endif
